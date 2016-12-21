c0547204894391fc5d72b80adef994e7a2336928
dialle11 <19410dia@sina.com>
2016-12-21 10:48:06 +0800

    在BooksContentActivity.class中添加了其布局的各种组件，评论按钮的监听器也加了，把Mainactivity.class改为了欢迎界面，添加了欢迎布局

--- src/com/itcast/booksale/BooksContentActivity.java
+++ src/com/itcast/booksale/BooksContentActivity.java
@@ -13,27 +13,44 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 
+/*
+ * 书的详情界面
+ */
 public class BooksContentActivity extends Activity {
 
 	List<Comment> comments;
 	int page = 0;
-	Comment_Listfragment fragComment;
+	Comment_Listfragment fragComment;            //Comment_Listfragment是用于展示图书的评论的
 	//	Article commentMess;
 	private Book book;
-	ListView commentListView;
-
+	ListView commentListView;              //
+	
+	private Button btn_subscribe;             //订阅按钮
+	private Button btn_massage;             //私信按钮
+	private Button btn_comment;              //底部评论按钮
+	
+	private TextView bookUserName;          //卖书卖家姓名
+	private TextView bookTitle;                 //图书标题
+	private TextView bookUserPhone;           //卖家电话号码
+	private TextView bookUserQQ;              //卖家qq
+	private TextView bookUserText;            //卖家备注
+	private TextView bookSummaryText;  
+	
+	private AvatarView bookUserAvatar;           //图书照片
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 
 		super.onCreate(savedInstanceState);
-
-
-
 		setContentView(R.layout.activity_books_view);
+		
+		
+		//获得书本信息
+		book = (Book) getIntent().getSerializableExtra("data");
 
 		fragComment = (Comment_Listfragment) getFragmentManager().findFragmentById(R.id.show_comment_list);
 
@@ -41,15 +58,8 @@ public class BooksContentActivity extends Activity {
 
 		book = (Book) getIntent().getSerializableExtra("data");//获取Book
 
-		TextView bookUserName = (TextView) findViewById(R.id.user_name);//卖家
-		TextView bookTitle = (TextView) findViewById(R.id.book_title);//书名
-		TextView bookUserPhone = (TextView) findViewById(R.id.book_user_phone);//卖家电话
-		TextView bookUserQQ = (TextView) findViewById(R.id.book_user_qq);//卖家QQ
-		TextView bookUserText = (TextView) findViewById(R.id.user_sell_text);//卖家留言
-		TextView bookSummaryText = (TextView) findViewById(R.id.text_about_book);//内容简介
 		
-		AvatarView bookUserAvatar = (AvatarView) findViewById(R.id.user_avatar);//卖家头像
-		
+		initmethod();            //初始化
 		
 		bookUserName.setText(book.getUser().getName());
 		bookTitle.setText(book.getTitle());
@@ -62,7 +72,7 @@ public class BooksContentActivity extends Activity {
 		fragComment.setBookId(book.getId().toString());//(book.getId().toString());
 
 		//评论
-		findViewById(R.id.btn_comment).setOnClickListener(new View.OnClickListener() {
+		btn_comment.setOnClickListener(new View.OnClickListener() {
 
 			@Override
 			public void onClick(View v) {
@@ -72,7 +82,7 @@ public class BooksContentActivity extends Activity {
 		});
 
 		//订阅
-		findViewById(R.id.btn_subscribe).setOnClickListener(new View.OnClickListener() {
+		btn_subscribe.setOnClickListener(new View.OnClickListener() {
 
 			@Override
 			public void onClick(View v) {
@@ -81,7 +91,7 @@ public class BooksContentActivity extends Activity {
 		});
 
 		//私信
-		findViewById(R.id.btn_massage).setOnClickListener(new View.OnClickListener() {
+		btn_massage.setOnClickListener(new View.OnClickListener() {
 
 			@Override
 			public void onClick(View v) {
@@ -91,6 +101,22 @@ public class BooksContentActivity extends Activity {
 		});
 
 	}
+	
+	//初始化
+	public void initmethod() {
+		 bookUserName = (TextView) findViewById(R.id.user_name);//卖家
+		 bookTitle = (TextView) findViewById(R.id.book_title);//书名
+		 bookUserPhone = (TextView) findViewById(R.id.book_user_phone);//卖家电话
+		 bookUserQQ = (TextView) findViewById(R.id.book_user_qq);//卖家QQ
+		 bookUserText = (TextView) findViewById(R.id.user_sell_text);//卖家留言
+		 bookSummaryText = (TextView) findViewById(R.id.text_about_book);//内容简介
+		
+		 bookUserAvatar = (AvatarView) findViewById(R.id.user_avatar);//卖家头像
+		 
+		 btn_subscribe=(Button) findViewById(R.id.btn_subscribe);             //订阅按钮
+		 btn_massage=(Button) findViewById(R.id.btn_massage);             //私信按钮
+		 btn_comment=(Button) findViewById(R.id.btn_comment);           //底部评论按钮
+	}
 
 
 	@Override
@@ -99,11 +125,10 @@ public class BooksContentActivity extends Activity {
 	}
 
 	void goCommentActivity(){
-		//把书本信息传到评论类
-		Book book = (Book) getIntent().getSerializableExtra("data");
+		
 
 		Intent itnt = new Intent(this,CommentActivity.class);
-		itnt.putExtra("data", book);
+		itnt.putExtra("data", book);                //把书的信息传给添加评论界面
 		startActivity(itnt);
 		finish();
 	}
--- src/com/itcast/booksale/MainActivity.java
+++ src/com/itcast/booksale/MainActivity.java
@@ -1,48 +1,78 @@
 package com.itcast.booksale;
 
+import java.io.IOException;
+
 import com.example.booksale.R;
+import com.itcast.booksale.servelet.Servelet;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
+import android.widget.Toast;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
+/*
+ * 首页，加了欢迎界面
+ */
 public class MainActivity extends Activity {
-
-
-	
-	
-	private Button btn_comment;        //评论按钮
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.mainactivity_comment);
+		setContentView(R.layout.welcome_view);
 
-		btn_comment=(Button) findViewById(R.id.btn_books_review);
-		btn_comment.setOnClickListener(new CommmentOnClickListener());
-		
-
-	}
-
-	class CommmentOnClickListener implements OnClickListener
-	{
-
-		@Override
-		public void onClick(View v) {
-			Intent intent=new Intent(MainActivity.this, CommentActivity.class);
-			startActivity(intent);
-			finish();
-		}
 		
 	}
+
+	
 	
 	@Override
 	protected void onResume() {
 		super.onResume();
 
+		//创建客户端连接
+		OkHttpClient client=Servelet.getOkHttpClient();
+		//创建请求
+		Request request=Servelet.requestuildApi("hello")
+				.method("get", null)
+				.build();
+		
+		
+		//客户端发起请求
+		client.newCall(request)
+		.enqueue(new Callback() {
+			
+			@Override
+			public void onResponse(Call arg0, Response arg1) throws IOException {
+				final String string=arg1.body().string();            //此为后台实现，不能在主线程中实现
+				MainActivity.this.runOnUiThread(new Runnable() {
+					
+					@Override
+					public void run() {
+						new AlertDialog.Builder(MainActivity.this)
+						.setTitle("登录成功")
+						.setMessage(string)
+						.setPositiveButton("确定", null)
+						.show();
+						
+						//跳转到主界面
+						Intent intent = new Intent(MainActivity.this, HelloWorldActivity.class);
+						startActivity(intent);
+						finish();
+					}
+				});
+			}
+			
+			@Override
+			public void onFailure(Call arg0, IOException arg1) {
+				Toast.makeText(MainActivity.this, "对不起你没有连接上", Toast.LENGTH_SHORT).show();
+			}
+		});
 		
 	}
 
