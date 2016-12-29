e05a14082a437a6cfc83eb56dec13e346fb93664
dialle11 <19410dia@sina.com>
2016-12-29 09:56:41 +0800

    在androidManifest.xml中注册了OrdersActivity.class 在Buy_book_bus_fragment里面增加了订单号

--- src/com/itcast/booksale/OrdersActivity.java
+++ src/com/itcast/booksale/OrdersActivity.java
@@ -8,5 +8,6 @@ public class OrdersActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+//		setContentView(R.layout.activity_orders_view);
 	}
 }
