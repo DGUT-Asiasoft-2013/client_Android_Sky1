74e3e2c644cc326945bcf4ff2ac95fa805c2e4c6
Clevercsc <1046839730@qq.com>
2016-12-30 10:50:11 +0800

    修改了我的-订单的跳转页面,稍完善了账单显示功能

--- src/com/itcast/booksale/SpendingBillsActivity.java
+++ src/com/itcast/booksale/SpendingBillsActivity.java
@@ -3,10 +3,12 @@ package com.itcast.booksale;
 import com.itcast.booksale.fragment.widgets.AvatarView;
 import com.itcast.booksale.servelet.Servelet;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.text.format.DateFormat;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.TextureView;
 import android.view.View;
@@ -43,7 +45,7 @@ public class SpendingBillsActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_spending_bills);
 		ListView billsList = (ListView) findViewById(R.id.list_bills_list);
-	headView = LayoutInflater.from(SpendingBillsActivity.this).inflate(R.layout.widget_billslist_month, null);
+		headView = LayoutInflater.from(SpendingBillsActivity.this).inflate(R.layout.widget_billslist_month, null);
 		billsList.addHeaderView(headView);
 		billsList.setOnItemClickListener(new OnItemClickListener() {
 			@Override
@@ -67,7 +69,7 @@ public class SpendingBillsActivity extends Activity {
 
 	// 订单详情列表的适配器
 	BaseAdapter adapter = new BaseAdapter() {
-
+		@SuppressLint("InflateParams")
 		@Override
 		public View getView(int position, View convertView, ViewGroup parent) {
 			if (convertView == null) {
@@ -79,12 +81,13 @@ public class SpendingBillsActivity extends Activity {
 			AvatarView avatar = (AvatarView) convertView.findViewById(R.id.avatar_bills_saler);// 订单的销售者
 			TextView textMonery = (TextView) convertView.findViewById(R.id.text_bills_monery);// 订单的金额
 			TextView textDetail = (TextView) convertView.findViewById(R.id.text_bills_detail);// 订单的简略详情
-		//	TextView textState = (TextView) convertView.findViewById(R.id.text_bills_date);// 订单的状态
+			// TextView textState = (TextView)
+			// convertView.findViewById(R.id.text_bills_date);// 订单的状态
 			TextView textMonth = (TextView) convertView.findViewById(R.id.text_bills_date_month);// FootBood的月份提示
 
 			// 为各个Item设定数值
 			textDate.setText(DateFormat.format("MM-dd", listData.getCreateDate()));
-			avatar.load(listData.getId().getUser());
+			avatar.load(listData.getUser());
 			textMonery.setText(String.valueOf(listData.getPayMoney()));
 
 			// 判断订单详情长度是否大于某值,大于则截取
@@ -93,22 +96,15 @@ public class SpendingBillsActivity extends Activity {
 			} else {
 				textDetail.setText(listData.getId().getBook().getSummary());
 			}
-/*
-			// 判断不同的订单状态
-			switch (listData.getFinish()) {
-			case state[0]:
-				textState.setText("待付款");
-				textState.setTextColor(0xD2691E);
-				break;
-			case state[1]:
-				textState.setText("交易关闭");
-				textState.setTextColor(0xD9D9D9);
-				break;
-			default:
-				textState.setText("");
-				break;
-
-			}*/
+			/*
+			 * // 判断不同的订单状态 switch (listData.getFinish()) { case state[0]:
+			 * textState.setText("待付款"); textState.setTextColor(0xD2691E);
+			 * break; case state[1]: textState.setText("交易关闭");
+			 * textState.setTextColor(0xD9D9D9); break; default:
+			 * textState.setText(""); break;
+			 * 
+			 * }
+			 */
 			textMonth.setText(DateFormat.format("MM月 ", listData.getCreateDate()));
 
 			return convertView;
@@ -140,29 +136,45 @@ public class SpendingBillsActivity extends Activity {
 		reload();
 	}
 
-	private void reload() {
-		Request request = Servelet.requestuildApi("orders").build();
+	void reload() {
+		Request request = Servelet.requestuildApi("orders").get().build();
 		Servelet.getOkHttpClient().newCall(request).enqueue(new Callback() {
 
 			@Override
 			public void onResponse(Call arg0, final Response arg1) throws IOException {
-
-				final Page<OrderLists> pageData = new ObjectMapper().readValue(arg1.body().string(), new TypeReference<Page<OrderLists>>() {
-				});
+				String dataRS = arg1.body().string();
+				Log.d("dataRS", dataRS);
+				try{
+				final Page <OrderLists> pageData = new ObjectMapper().readValue(dataRS,
+						new TypeReference<Page<OrderLists>>() {
+						});
 				runOnUiThread(new Runnable() {
-					
+
 					@Override
 					public void run() {
 						SpendingBillsActivity.this.data = pageData.getContent();
+
 						adapter.notifyDataSetChanged();
 					}
 				});
+				}catch (Exception e) {
+					runOnUiThread(new Runnable() {
+
+						@Override
+						public void run() {
+							Toast.makeText(SpendingBillsActivity.this, "请求账单列表失败了", Toast.LENGTH_SHORT).show();
+						}
+					});
+					e.printStackTrace();
+					
+				}
+				
 			}
 
 			@Override
 			public void onFailure(Call arg0, IOException arg1) {
 				runOnUiThread(new Runnable() {
-					
+
 					@Override
 					public void run() {
 						Toast.makeText(SpendingBillsActivity.this, "请求账单列表失败了", Toast.LENGTH_SHORT).show();
@@ -174,12 +186,10 @@ public class SpendingBillsActivity extends Activity {
 	}
 
 	public void goToOrderList(int position) {
-		
 
 	}
 
 	public void deleteBillList(int postition) {
 
-		
 	}
 }
